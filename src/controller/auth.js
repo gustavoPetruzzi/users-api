5a2f730db39f7b889f78d02e6370a51d253f849c
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-05-27 00:26:15 -0300

    Add login controller

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -1,6 +1,7 @@
 import * as argon2 from 'argon2';
 
-import { checkIfUserExists, createUser } from "../db/queries.js";
+import { checkIfUserExists, createUser, getUserByEmail } from "../db/queries.js";
+import { getSignedToken } from '../util.js';
 
 export const signUp = async (req, res) => {
   try {
@@ -31,6 +32,44 @@ export const signUp = async (req, res) => {
     console.log(error)
     res.status(500).send({
       error: "An error has occurred"
+    });
+  }
+}
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await getUserByEmail(email);
+    
+    if (!user) {
+      return res.status(400).send({
+        message: "An error has occurred"
+      });
+    }
+    const isCorrectPassword = await argon2.verify(
+      user.password,
+      password,
+      { secret: Buffer.from(process.env.PEPPER) }
+    );
+
+    if (!isCorrectPassword) {
+      return res.status(400).send({
+        message: "An error has occurred"
+      });
+    }
+
+    const signedToken = getSignedToken({id: user.user_id, email: user.email });
+    res.cookie('x-session-token', JSON.stringify(signedToken), { httpOnly: true, maxAge: 3600000 })
+
+    res.status(200).send({
+      message: "Success"
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "An error has ocurred"
     });
   }
 }
