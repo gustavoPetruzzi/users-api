b06329250eb677311d2eb013f3b38c9b3e1f6593
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-05-26 22:31:42 -0300

    Remove unused endpoints

--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/", (req, res) => {
-  res.send("Hello world")
-});
-
-app.get('/env', (req, res) => {
-  res.json(process.env);
-})
-
 app.use(authRoutes);
 
 app.listen(PORT, () => {
