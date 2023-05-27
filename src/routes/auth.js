3a82206562fa1f76ce465d312750ed4d127d03d4
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-05-27 00:36:45 -0300

    Add route for logout

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { signUp, login } from '../controller/auth.js';
+import { signUp, login, logout } from '../controller/auth.js';
 import { isValidEmail } from '../middleware/is-valid-email.js';
 const router = express.Router();
 
 router.post('/signup', isValidEmail, signUp);
 
 router.post('/login', isValidEmail, login);
+
+router.get('/logout', logout);
+
 export default router;
