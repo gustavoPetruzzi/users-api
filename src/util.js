8f24a28514aad68c5bebf4be53b20b3b2ade8175
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-05-26 23:50:50 -0300

    Add function to getSignedToken

---
+++ src/util.js
@@ -0,0 +1,12 @@
+import jsonwebtoken from "jsonwebtoken";
+
+/**
+ * 
+ * @param {Object} user         User's data that is going to be use in the JWT
+ * @param {string} user.id      User's id
+ * @param {string} user.email   User's email
+ * @returns {string}            A signed jwt
+ */
+export const getSignedToken = (user) => {
+  return jsonwebtoken.sign(user, process.env.JWT_KEY);
+}
